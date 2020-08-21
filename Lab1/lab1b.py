906942d9fb9fe3562680c17fd78f395215348eb8
nixholas <nixholas@icloud.com>
2020-08-21 14:12:42 +0800

    lab1b: Recursion is huge, but it makes it go round properly
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Lab1/lab1b.py
+++ Lab1/lab1b.py
@@ -6,7 +6,8 @@ import sys
 
 # All statements should only be in functions.
 def gcd_b(x, y):
+    sys.setrecursionlimit(5000)
     # For two integers x and y, if x > y then gcd(x, y) = gcd(x - y, y)
     if x > y: return gcd_b(x - y, y)
     elif x < y: return gcd_b(x, y - x)
-    return x
+    else: return x
