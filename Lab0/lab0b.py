c6e4a72be59790cc4cfe6afa27e7fcfda934e5d6
nixholas <nixholas@icloud.com>
2020-08-21 11:22:01 +0800

    Lab0: lab0b: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Lab0/lab0b.py
+++ Lab0/lab0b.py
@@ -7,4 +7,13 @@
 # fill up the weight_category method to return either "underweight", "overweight" or "normal" 
 # depending on the height (in cm) and weight (in kg)
 def weight_category(weight, height):
-  return "dummy_value"
+    bmi = weight / (height ** height)
+    if bmi <= 0:
+        return "incorrect weight/height value!"
+
+    if bmi < 18.5:
+        return "underweight"
+    elif 18.5 <= bmi <= 25:
+        return "normal"
+    else:
+        return "overweight"
