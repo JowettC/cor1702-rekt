ed25c372889f2268a69534405dc627b39dbb4730
nixholas <nixholas@icloud.com>
2020-08-21 14:28:18 +0800

    lab1c: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Lab1/lab1c.py
+++ Lab1/lab1c.py
@@ -5,4 +5,8 @@
 
 # All statements should only be in functions.
 def gcd_c(x, y):
-  return 0
+    # Adapted from lab1a, I apparently wrote this instead of brute force in 1a
+    # if y = 0, we got the answer. if y is not 0, we get the remainder of x / y to get the next remainder
+    # we use the remainder to check if x can be equally divided. Since when x % y = 0, we can recurse this
+    # equation to obtain the result.
+    return x if y == 0 else gcd_c(y, x % y)
