11eb7af46b54529c9c27700dffbe4c473425efb6
nixholas <nixholas@icloud.com>
2020-08-21 14:06:40 +0800

    lab1b: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Lab1/lab1b.py
+++ Lab1/lab1b.py
@@ -2,7 +2,11 @@
 # Section: G7
 
 # lab1b (Dijkstra's algo)
+import sys
 
 # All statements should only be in functions.
 def gcd_b(x, y):
-  return 0
+    # For two integers x and y, if x > y then gcd(x, y) = gcd(x - y, y)
+    if x > y: return gcd_b(x - y, y)
+    elif x < y: return gcd_b(x, y - x)
+    return x
