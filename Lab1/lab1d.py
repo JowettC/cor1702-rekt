652063ae27f16bb778c0f7c7e633bf6c3f76e515
nixholas <nixholas@icloud.com>
2020-08-21 14:52:36 +0800

    lab1d: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Lab1/lab1d.py
+++ Lab1/lab1d.py
@@ -5,4 +5,21 @@
 
 # All statements should only be in functions.
 def gcd_d(x, y):
-  return 0
+    k = 0
+
+    while x % 2 == 0 and y % 2 == 0:  # while x and y are both even
+        x /= 2
+        y /= 2
+        k += 1
+
+    while x != y:
+        if x % 2 == 0:
+            x /= 2  # if x is even then x /= 2
+        elif y % 2 == 0:
+            y /= 2  # if y is even then y /= 2
+        elif x > y:
+            x = (x - y) / 2 # if x > y then x = (x - y) / 2
+        else:
+            y = (y - x) / 2 # else y = (y - x) / 2
+
+    return x * 2 ** k
