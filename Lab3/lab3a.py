16a749046ef2e43080157ced97db7fd2bb879e8c
nixholas <nixholas@icloud.com>
2020-09-18 01:13:15 +0800

    Lab3: Initial import
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Lab3/lab3a.py
@@ -0,0 +1,14 @@
+# Name: Nicholas Chen Han Wei
+# Section: G7
+
+# lab3a
+
+# All statements should only be in functions. Do not include statements outside functions in this file.
+
+
+# Takes in an integer and returns the sum of all its digits as an integer
+# this function does NOT have to handle negative numbers (i.e. i will always be >=0)
+# this function must be recursive (i.e. it will call itself)
+# Refer to hints in the requirements doc.
+def sum_of_digits(i):
+  return i if i == 0 else i % 10 + sum_of_digits(i // 10)
