b89d448db8ffbf95605a99b4b9fc5d52b96742d2
nixholas <nixholas@icloud.com>
2020-10-11 21:36:53 +0800

    Lab4: Initial import
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Lab4/lab4.py
@@ -0,0 +1,17 @@
+# Name: Nicholas Chen Han Wei
+# Section: G7
+
+# lab4
+
+# All statements should only be in functions. Do not include statements outside functions in this file.
+
+def select_tweeters(followers):
+    sorted_followers = []
+
+    for f in followers:
+        if len(sorted_followers) > 0:
+            sorted_followers.append(f)
+        else:
+            sorted_followers.append(f)
+
+    return sorted_followers[]
