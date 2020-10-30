eec43f95e2580e622daccf39e2dbf9242ee9215e
nixholas <nixholas@icloud.com>
2020-10-30 15:36:14 +0800

    PaperAssignm: GraphLab: Q4a: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- PaperAssgnm/test.py
+++ PaperAssgnm/test.py
@@ -1,6 +1,27 @@
-from GraphLab import *
+from PaperAssgnm.GraphLab import *
 
-g = GraphLab.UndirectedGraph()
+g = UndirectedGraph()
 
-for v in g.vertices:
-    print(v)
+nicholas = Vertex(1)
+bing = Vertex(2)
+joshua = Vertex(3)
+john = Vertex(4)
+jenny = Vertex(5)
+gina = Vertex(6)
+
+"""
+Let's say, 
+
+Nicholas is friends with bing and joshua
+John is friends with Joshua
+Joshua is friends with Gina and Jenny
+"""
+g.addEdge(nicholas, bing)
+g.addEdge(nicholas, joshua)
+g.addEdge(john, joshua)
+g.addEdge(joshua, gina)
+g.addEdge(joshua, jenny)
+g.addEdge(jenny, bing)
+g.addEdge(nicholas, gina)
+
+print(bfs_traversal(nicholas))
